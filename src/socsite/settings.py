131cd6c1104ad7136fc83ce88b3368195a036cd1
devilnotalone <devilnotalone@gmail.com>
2024-11-07 10:49:39 +0700

    commit-7-11-2024

--- src/socsite/settings.py
+++ src/socsite/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'news',
     'course',
     'student',
-    'accounts'
+    'accounts',
+    'mediafile'
     
 ]
 
--- src/socsite/views.py
+++ src/socsite/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from pages.models import Slide,Page
+from django.shortcuts import render, get_object_or_404
+from pages.models import Slide
+from mediafile.models import MediaFile
 
 def home_view(request):
+  media_file = get_object_or_404(MediaFile, id=2)
+  media_file_2 = get_object_or_404(MediaFile, id=3)
   slides = Slide.objects.order_by('-order')[:3]
-  return render(request, "pages/home.html", {'slides' : slides})
+  return render(request, "site/index.html", {'slides' : slides, 'media_file': media_file, 'media_file_2': media_file_2 })
 
 """ def slide(request):
     slides = Slide.objects.order_by('order')  # ดึงสไลด์สามรายการล่าสุด
